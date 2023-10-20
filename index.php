14974582ecd19a348b47f18e71b241cee6651651
ShibliRaihan <shibli.raihan.bangli@gmail.com>
2023-10-21 00:53:35 +0600

    intearface

--- index.php
+++ index.php
@@ -1,21 +1,42 @@
 <?php
-class Student
-{
-    public $name;
-    function students($text)
-    {
-        echo $this->name = $text;
-    }
-}
+// class Student
+// {
+//     public $name;
+//     function students($text)
+//     {
+//         echo $this->name = $text;
+//     }
+// }
 
-class Teacher extends Student
-{
-    function teachers()
-    {
-        $this->name;
-    }
-}
+// class Teacher extends Student
+// {
+//     function teachers()
+//     {
+//         $this->name;
+//     }
+// }
 
-$show_name = new Teacher();
-$show_name->students("h");
+// $show_name = new Teacher();
+// $show_name->students("h");
 
+#############################interface#############################
+
+
+// interface it_manager {
+//     function at_name();
+//     function tt_name();
+// }
+
+// class my_ implements it_manager {
+//     function at_name() {
+//         echo "at_name";
+//     }
+//     function tt_name() {
+//         echo "\ntt_name";
+        
+//     }
+// }
+
+// $my = new my_();
+// $my->at_name();
+// $my->tt_name();
